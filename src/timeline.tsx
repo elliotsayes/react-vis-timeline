876e60529a5abe2b97b0fadae0c2a34a341f4633
Elliot Sayes <elliot@sayes.cloud>
2023-07-30 18:52:33 +0800

    Revert "Switch around"
    This reverts commit bebcb194f525d2295e13a7f418a2249b7726852d.

--- src/timeline.tsx
+++ src/timeline.tsx
@@ -99,8 +99,6 @@ export class Timeline extends Component<Props, {}> {
 	}
 
 	componentDidMount() {
-		const { initialItems, initialGroups, selection, selectionOptions = {} } = this.props;
-
 		if (this.groups.length === 0 && initialGroups?.length > 0) {
 			this.groups.add(initialGroups);
 		}
@@ -191,7 +189,16 @@ export class Timeline extends Component<Props, {}> {
 	}
 
 	init() {
-		const { options, customTimes, animate = true, currentTime } = this.props;
+		const {
+			initialItems,
+			initialGroups,
+			options,
+			selection,
+			selectionOptions = {},
+			customTimes,
+			animate = true,
+			currentTime
+		} = this.props;
 
 		let timelineOptions = options;
 
